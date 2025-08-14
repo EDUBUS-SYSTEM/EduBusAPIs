411dc46fd401b9d69febc2f4cd9cf944f3ef0833
nguyet <bebyheomap0810@gmail.com>
2025-08-14 19:58:06 +0700

    Configure DB connections and register services

--- APIs/Program.cs
+++ APIs/Program.cs
@@ -1,8 +1,37 @@
+﻿using Data;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// SQL Server EF Core
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DevConnection"),
+        sqlOptions => sqlOptions.EnableRetryOnFailure()) // retry policy
+);
+
+// MongoDB
+builder.Services.AddSingleton<IMongoClient>(sp =>
+{
+    // Get Passowrd from configuration (User Secrets will override appsettings)
+    var mongoPassword = builder.Configuration["MongoPassword"];
+
+    // Get string connection từ appsettings
+    var connStrTemplate = builder.Configuration.GetConnectionString("MongoConnection");
+
+    // Replace placeholder with real password
+    var connStr = connStrTemplate.Replace("{MongoPassword}", mongoPassword);
+
+    return new MongoClient(connStr);
+});
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ Services/TestService.cs
@@ -0,0 +1,39 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class TestService
+    {
+        private readonly IMongoClient _mongoClient;
+
+        public TestService(IMongoClient mongoClient)
+        {
+            _mongoClient = mongoClient;
+        }
+
+        public async Task TestInsert()
+        {
+            var database = _mongoClient.GetDatabase("EduBus");
+            var collection = database.GetCollection<BsonDocument>("test");
+
+            var doc = new BsonDocument
+        {
+            { "message", "Hello from DI" },
+            { "createdAt", DateTime.UtcNow }
+        };
+
+            await collection.InsertOneAsync(doc);
+
+            var result = await collection.Find(new BsonDocument()).FirstOrDefaultAsync();
+            Console.WriteLine(result.ToJson());
+        }
+    }
+
+}
