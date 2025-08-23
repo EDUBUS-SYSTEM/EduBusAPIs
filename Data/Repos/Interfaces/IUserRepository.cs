e793d54f3f92f676419d4d1324f2bcf153b7bba3
Tan Chun <nguyenvietthanhtrunga3@gmail.com>
2025-08-23 20:07:41 +0700

    complete esbms-65 task
    - create parent
    - create driver
    - export parent, driver list excel file

---
+++ Data/Repos/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repos.Interfaces
+{
+    public interface IUserRepository:ISqlRepository<UserAccount>
+    {
+        Task<bool> IsEmailExistAsync(string email);
+        Task<UserAccount?> FindByEmailAsync(string email);
+    }
+}
